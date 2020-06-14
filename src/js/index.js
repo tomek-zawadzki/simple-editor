f6886e6eae4c761a12fff16e30d32a7238e6e777
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-06-14 21:50:55 +0200

    add function in JS/ use localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,5 +6,22 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
 
+const textArea = document.querySelector(".editor__textarea--js");
+const buttonLoad = document.querySelector(".editor__button-load--js");
+const buttonSave = document.querySelector(".editor__button-save--js");
+
+const entry = localStorage.getItem("entry", textArea.value);
+
+if (textArea.value) {
+  textArea.value = entry;
+}
+
+buttonSave.addEventListener("click", () => {
+  localStorage.setItem("entry", textArea.value);
+});
+
+buttonLoad.addEventListener("click", () => {
+  textArea.value = localStorage.getItem("entry", textArea.value);
+});
